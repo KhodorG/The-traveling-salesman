e5ebfbdfb4800d48387f76d1a36e769846342976
KhodorG <126386447+KhodorG@users.noreply.github.com>
2023-05-07 19:03:56 +0300

    Update and rename OPENMP.c to OpenMP.c

--- OpenMP.c
+++ OpenMP.c
@@ -3,9 +3,11 @@
 #include <omp.h>
 #include <time.h>
 
-#define MAX_CITIES 1000
+#define MAX_CITIES 100000
+#define MAX_DISTANCE 100
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int i, j, n, total_cost = 0, cost;
     int **cities;
     int min_cost = 99999999;
@@ -15,49 +17,57 @@ int main(int argc, char *argv[]) {
     printf("Enter the number of cities (max %d): ", MAX_CITIES);
     scanf("%d", &n);
 
-    if (n > MAX_CITIES) {
+    if (n > MAX_CITIES)
+    {
         printf("Error: number of cities exceeds maximum allowed.\n");
         exit(1);
     }
 
-    printf("Randomly generating distances between cities...\n");
-
-    omp_set_num_threads(4);
+    omp_set_num_threads(8);
 
     // Allocate cities array on the heap
     cities = (int **)malloc(n * sizeof(int *));
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         cities[i] = (int *)malloc(n * sizeof(int));
     }
 
-    #pragma omp parallel private(i, j, cost)
+#pragma omp parallel private(i, j, cost)
     {
         int visited[MAX_CITIES] = {0};
-        unsigned int seed = time(NULL) + omp_get_thread_num();
 
-        #pragma omp for collapse(2)
-        for (i = 0; i < n; i++) {
-            for (j = i; j < n; j++) {
-                if (i == j) {
+#pragma omp for schedule(static)
+        for (i = 0; i < n; i++)
+        {
+            for (j = i; j < n; j++)
+            {
+                if (i == j)
+                {
                     cities[i][j] = 0;
-                } else {
-                    cities[i][j] = cities[j][i] = rand_r(&seed) % 100;
+                }
+                else
+                {
+                    cities[i][j] = cities[j][i] = MAX_DISTANCE;
                 }
             }
         }
 
-        #pragma omp for reduction(min:min_cost)
-        for (i = 0; i < n; i++) {
+#pragma omp for reduction(min : min_cost)
+        for (i = 0; i < n; i++)
+        {
             visited[i] = 1;
             cost = 0;
 
-            for (j = 0; j < n; j++) {
-                if (!visited[j]) {
+            for (j = 0; j < n; j++)
+            {
+                if (!visited[j])
+                {
                     cost += cities[i][j];
                 }
             }
 
-            if (cost < min_cost) {
+            if (cost < min_cost)
+            {
                 min_cost = cost;
             }
 
@@ -73,7 +83,8 @@ int main(int argc, char *argv[]) {
     printf("Time taken: %f seconds\n", end_time - start_time);
 
     // Free cities array memory
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         free(cities[i]);
     }
     free(cities);
